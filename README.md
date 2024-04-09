201768da11ee0ea9ca6db4982957c0fa4d4d1625
Tien Thanh Nguyen <tienthanh.nguyen@mil.be>
2024-04-09 11:42:16 +0200

    add content from commit: 8824eb7835d5f0d7dfc30781985b090091440198 of https://github.com/ctu-mrs/simulation with adaption for gimbals for x500

---
+++ README.md
@@ -0,0 +1,58 @@
+# The Old Gazebo Simulation
+
+> :warning: **Attention please: This package is no longer maintained!!!**
+>
+> This is an old package that is no longer maintained.
+> The new version of the Gazebo Simulation is located [here](https://github.com/ctu-mrs/mrs_uav_gazebo_simulation).
+> This repository will be erased soon
+
+## Submodules
+
+| ROS Package                 |
+|-----------------------------|
+| [mrs_simulation](https://github.com/ctu-mrs/mrs_simulation)              |
+| [mrs_gazebo_common_resources](https://github.com/ctu-mrs/mrs_gazebo_common_resources) |
+| [px4](https://github.com/ctu-mrs/px4_firmware)                         |
+| [mavlink_sitl_gazebo](https://github.com/ctu-mrs/px4_sitl_gazebo)         |
+
+## System requirements
+
+Required OS is Ubuntu 20.04 LTS 64-bit or its flavors that can install ROS Noetic.
+The suggested variant of OS installation is dual boot instead of virtualization that can be slow and can not handle well the simulation GUI.
+We use [Gitman](https://github.com/jacebrowning/gitman) to manage the repository **submodules**.
+The repository are supposed to be compiled by [catkin tools](https://catkin-tools.readthedocs.io).
+
+## Unmanned Aerial Vehicles
+
+| Model        | Spawn argument | Simulation |
+|--------------|----------------|------------|
+| DJI f330     | `--f330`         | ![](.fig/f330_simulation.jpg)     |
+| DJI f450     | `--f450`         | ![](.fig/f450_simulation.jpg)     |
+| Holybro x500 | `--x500`         | ![](.fig/x500_simulation.jpg)     |
+| DJI f550     | `--f550`         | ![](.fig/f550_simulation.jpg)     |
+| Tarot t650   | `--t650`         | ![](.fig/t650_simulation.jpg)     |
+| NAKI II      | `--naki`         | ![](.fig/naki_simulation.jpg)     |
+
+## Examples of tmuxinator simulation sessions
+
+Selected tmuxinator scripts:
+
+- [example_tmux_scripts/one_drone_gps](example_tmux_scripts/one_drone_gps)
+- [example_tmux_scripts/one_drone_gps_standalone](example_tmux_scripts/one_drone_gps_standalone)
+- [example_tmux_scripts/one_drone_optic_flow](example_tmux_scripts/one_drone_optic_flow)
+- [example_tmux_scripts/two_drones_gps](example_tmux_scripts/two_drones_gps)
+
+Bare tmux script (similar to the one used on real UAVs):
+
+- [example_tmux_scripts/just_flying_bare_tmux](example_tmux_scripts/just_flying_bare_tmux)
+
+For detail description of script capabilities for spawning vehicles see [mrs_simulation](https://github.com/ctu-mrs/mrs_simulation).
+
+## Finishing your .bashrc
+
+The `install.sh` script will add the following to your .bashrc:
+
+```bash
+source /opt/ros/<ros_version>/setup.bash
+source /usr/share/gazebo/setup.sh
+```
---
+++ installation/install.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+## | ----------------- trapping !=0 exitcodes ----------------- |
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
+debian=`lsb_release -d | grep -i debian | wc -l`
+[[ "$debian" -eq "1" ]] && ROS_DISTRO="noetic" && distro="20.04" && DEBIAN=true
+
+## | ----------- go to the directory of this script ----------- |
+
+cd "$MY_PATH"
+
+## | ----------------------- install git ---------------------- |
+
+sudo apt-get -y install git
+
+## | ----------------------- install ROS ---------------------- |
+
+$MY_PATH/dependencies/ros.sh
+
+## | --------------------- install git lfs -------------------- |
+
+$MY_PATH/dependencies/git_lfs.sh
+
+## | --------------------- install gitman --------------------- |
+
+$MY_PATH/dependencies/gitman.sh
+
+## | ---------------- install gitman submodules --------------- |
+
+gitman install --force
+
+## | ---------------- install px4 dependencies ---------------- |
+
+if [ -e /home/$USER/.local/lib ]; then
+  sudo chown $USER /home/$USER/.local/lib -R
+fi
+
+[ ! $DEBIAN ] && $MY_PATH/../ros_packages/px4_firmware/Tools/setup/ubuntu.sh --no-nuttx --no-sim-tool
+[ $DEBIAN ] && $MY_PATH/../ros_packages/px4_firmware/Tools/setup/debian.sh --no-nuttx --no-sim-tool
+
+sudo apt-get -y update
+
+# to fix mrs_gazebo_common_resources build on Ubuntu 20.04
+# sudo apt-get upgrade -y libignition-common3*
+
+if [ "$distro" = "18.04" ]; then
+  sudo -H pip install --user packaging toml
+  sudo apt-get -y install python-packaging python-toml
+
+  #hotfix for missing library in ubuntu 18.04 for mavlink_sitl_gazebo
+  sudo apt-get -y update --fix-missing
+  sudo apt-get -y install -f
+  sudo apt-get -y install libignition-math4
+  sudo apt-get -y upgrade libignition-math4
+
+elif [ "$distro" = "20.04" ]; then
+  sudo -H pip3 install --user packaging
+  sudo apt-get -y install python3-packaging
+else
+  echo -e "\e[31mThis version of Ubuntu (${distro}) is untested. Modify this script accordingly.\e[0m"
+  exit 1
+fi
+
+sudo apt-get -y install python3-toml
+sudo -H pip3 install --user toml
+
+# needed for the UAV spawner
+# sudo -H pip3 install --user defusedxml
+
+# [ "$distro" = "18.04" ] && sudo apt-get -y upgrade 'libignition-fuel-*' # this was needed after the first installation to fix the libignition-fuel-...
+
+sudo apt-get -y install 'libgstreamer1.0-dev' # needed for sitl_gazebo
+
+# # we need to find out why is this needed
+# sudo apt-get -y install \
+#   libignition-common3-graphics-dev
+
+## | --------------- add ROS sourcing to .bashrc -------------- |
+
+line="source /opt/ros/$ROS_DISTRO/setup.bash"
+
+num=`cat ~/.bashrc | grep "$line" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding '$line' to your .bashrc"
+
+  # set bashrc
+  echo "
+$line" >> ~/.bashrc
+
+fi
+
+## | ------------- add Gazebo sourcing to .bashrc ------------- |
+
+line="source /usr/share/gazebo/setup.sh"
+
+num=`cat ~/.bashrc | grep "$line" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding '$line' to your .bashrc"
+
+  # set bashrc
+  echo "
+$line" >> ~/.bashrc
+
+fi
